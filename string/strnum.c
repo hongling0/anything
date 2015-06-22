07c3f8a9687feb9f586a429e03d8fec7472411ec
hong <hongling-0@qq.com>
2015-06-22 09:47:28 +0800

    strnum

---
+++ string/strnum.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+double str2double(const char* str, double def)
+{
+  char* endptr = NULL;
+  double n = strtod(str, &endptr);
+  if (*endptr != '\0') {
+    fprintf(stderr, "can't convert to double %s\n", str);
+    return def;
+  }
+  return n;
+}
+long str2integer(const char* str, long def)
+{
+  char* endptr = NULL;
+  long n = strtol(str, &endptr, 0);
+  if (*endptr != '\0') {
+    fprintf(stderr, "can't convert to long %s\n", str);
+    return def;
+  }
+  return n;
+}
