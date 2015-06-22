e63266eab2a945498063aa9b59c6930ced08cada
hong <hongling-0@qq.com>
2015-06-22 10:17:45 +0800

    assert

--- string/strnum.c
+++ string/strnum.c
@@ -1,8 +1,10 @@
+#include <assert.h>
 #include <stdio.h>
 
 double str2double(const char* str, double def)
 {
   char* endptr = NULL;
+  assert(str);
   double n = strtod(str, &endptr);
   if (*endptr != '\0') {
     fprintf(stderr, "can't convert to double %s\n", str);
@@ -13,6 +15,7 @@ double str2double(const char* str, double def)
 long str2integer(const char* str, long def)
 {
   char* endptr = NULL;
+  assert(str);
   long n = strtol(str, &endptr, 0);
   if (*endptr != '\0') {
     fprintf(stderr, "can't convert to long %s\n", str);
