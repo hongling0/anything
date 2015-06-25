aee106e07e5d60b01658c1d026c5a42284f1a1b5
hong <hongling-0@qq.com>
2015-06-25 20:54:45 +0800

    epoll add del wait

--- net/poller.c
+++ net/poller.c
@@ -8,18 +8,42 @@
 #include <unistd.h>
 
 
-struct poller {
-  int efd;
+struct node{
+  struct node* next;
+  uint32_t id;
+  int fd;
+  int flag;
+  void(*on_event)(struct node*,int event);
 };
 
+struct poller {
+  int efd;
+  struct node* node_hash;
+  unsigned node_max;
+  unsigned node_now;
+};
+
+static uint32_t id_hash(uint32_t key){
+  key+=~(key<<15);
+  key^=(key>>10);
+  key+=(key<<3);
+  key^=(key>>6);
+  key+=~(key<<11);
+  key^=(key>>16);
+  return key;
+}
 
 
 struct poller* poller_create() {
   struct poller * p=(struct poller *)malloc(sizeof(*p));
+  p->node_max=1024;
+  p->node_now=0;
+  p->node_hash=(struct node*)malloc(sizeof(*p->node_hash)*p->node_max);
+  memset(p->node_hash,0,sizeof((*p->node_hash)*p->node_max));
   p->efd=epoll_create(1);
   if(p->efd==-1){
-    free(p);
     fprintf(stderr,"%s failure! %s\n",__func__,strerror(errno));
+    free(p);
     return NULL;
   }
   return p;
@@ -27,7 +51,64 @@ struct poller* poller_create() {
 
 void poller_delete(struct poller * p){
   close(p->efd);
+  free(p->node_hash);
   free(p);
+}
+
+int poller_addnode(struct poller*p,struct node* n){
+  struct epoll_event e;
+  e.events=n->flag;
+  e.data.ptr=n;
+  if(epoll_ctl(p->efd,EPOLL_CTL_ADD,n->fd,&e)==-1){
+    fprintf(stderr,"%s failure! %s\n",__func__,strerror(errno));
+    return -1;
+  }
+
+  uint32_t hash=id_hash(n->id)^(p->node_max-1);
+  n->next=p->node_hash[hash];
+  p->node_hash[hash]=n;
+  return 0;
+}
+
+int poller_removenode(struct *p,struct node* n){
+  int32_t hash=id_hash(n->id)^(p->node_max-1);
+  struct node* head=p->node_hash[hash];
+  struct node* prev,cur;
+  for(cur=head;cur;prev=cur,cur=cur->next){
+    if(cur==n){
+      break;
+    }
+  }
+  if(cur!=n){
+    return -1;
+  }
+  if(cur==head){
+    p->node_hash[hash]=cur->next;
+  }else{
+    prev->next=cur->next;
+  }
+  cur->next=NULL;
+
+  struct epoll_event e;
+  e.events=n->flag;
+  e.data.ptr=n;
+  if(epoll_ctl(p->efd,EPOLL_CTL_DEL,n->fd,&e)==-1){
+    fprintf(stderr,"%s failure! %s\n",__func__,strerror(errno));
+  }
+  return 0;
+}
+
+int poller_once(struct poller* p){
+  struct epoll_event ev[64];
+  int ret=epoll_wait(p->efd,ev,64,-1);
+  if(ret>0){
+    int i=0;
+    for(i=0;i<ret;i++){
+      struct epoll_event* e=&ev[i];
+      struct node* n=e.data.ptr;
+      n->on_event(n,e->events);
+    }
+  }
 }
 
 static int open_listen(const char* host,int port,int backlog){
@@ -91,9 +172,12 @@ static int open_connect(const char* host,int port){
     setsockopt(sock,SOL_SOCKET,SO_KEEPALIVE,(void*)&keepalive,sizeof(keepalive));
     int flag=fcntl(sock,F_GETFL,0);
     if(flag==-1){
-
+      goto sockerr;
     }
-    fcntl(sock,F_SETFL,flag|O_NONBLOCK);
+    flag=fcntl(sock,F_SETFL,flag|O_NONBLOCK);
+    if(flag==-1){
+      goto sockerr;
+    }
     statuc=connect(sock,ai_ptr->ai_addr,ai_ptr->ai_addrlen);
     if(status!=0&&errno!=EINPROGRESS){
       close(sock);
@@ -104,6 +188,8 @@ static int open_connect(const char* host,int port){
   }
   freeaddrinfo(ai_list);
   return sock;
+sockerr:
+  close(sock);
 failed:
   freeaddrinfo(ai_list);
   return -1;
